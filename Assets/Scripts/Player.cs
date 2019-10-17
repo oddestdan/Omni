126e128fde75d11497ddcc95855a5646ce6888b2
Dan Stetsenko <denyast@ukr.net>
2019-10-17 14:17:28 +0300

    Player movement and rotation to mouse cursor.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviour {
+    public float moveSpeed = 5;
+    PlayerController controller;
+
+    Camera viewCamera;
+
+    void Start() {
+        controller = GetComponent<PlayerController>();
+        viewCamera = Camera.main;
+    }
+
+    void Update() {
+        // Player movement
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+        controller.Move(moveVelocity);
+
+        // Ray from camera to mouse cursor
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray, out rayDistance)) {
+            Vector3 intersectionPoint = ray.GetPoint(rayDistance);
+            // Debug.DrawLine(ray.origin, intersectionPoint, Color.red);
+            controller.LookAt(intersectionPoint);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+    Rigidbody myRigidbody;
+    Vector3 velocity;
+
+    void Start() {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void FixedUpdate() {
+        myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+
+    public void Move(Vector3 _velocity) {
+        velocity = _velocity;
+    }
+
+    public void LookAt(Vector3 point) {
+        Vector3 sameHeightPoint = new Vector3(point.x, transform.position.y, point.z);
+        transform.LookAt(sameHeightPoint);
+    }
+}
