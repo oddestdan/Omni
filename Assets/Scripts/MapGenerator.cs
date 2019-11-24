d9ceaf2093cb80945f7b6ce39aabb30d8e001c58
Dan Stetsenko <denyast@ukr.net>
2019-11-24 22:36:52 +0200

    Minor bugfixes

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -174,8 +174,8 @@ public class MapGenerator : MonoBehaviour {
     public Transform GetTileFromPosition(Vector3 position) {
         int x = Mathf.RoundToInt(position.x / tileSize + (currentMap.mapSize.x - 1) / 2f);
         int y = Mathf.RoundToInt(position.z / tileSize + (currentMap.mapSize.y - 1) / 2f);
-        x = Mathf.Clamp(x, 0, tileMap.GetLength(0));
-        y = Mathf.Clamp(y, 0, tileMap.GetLength(1));
+        x = Mathf.Clamp(x, 0, tileMap.GetLength(0) - 1);
+        y = Mathf.Clamp(y, 0, tileMap.GetLength(1) - 1);
         return tileMap[x, y];
     }
 
